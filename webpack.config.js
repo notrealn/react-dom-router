f7342e9167c220671916345e974eb689aeaf470c
Glitch (hello-express) <none>
2020-06-23 15:17:00 +0000

    🏄🌦 Checkpoint ./webpack.config.js:7570007/1060 ./server.js:7570007/678

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  entry: "/public/script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: process.env.PORT
+  }
+};
